5d4bc2d313f7ba8d190ebb92ebade70ddb727f71
HarshitSharma2250 <sharma.harshit295@gmail.com>
2025-11-11 16:09:02 +0530

    mt-added

--- components/Layout/NavBar.tsx
+++ components/Layout/NavBar.tsx
@@ -38,7 +38,7 @@ function HandleLogput(){
 
 
   return (
-    <header className="w-full bg-white flex items-center justify-between px-6 py-3 shadow-[0_4px_4px_-2px_rgba(0,0,0,0.1)]  ">
+    <header className="w-full bg-white flex items-center justify-between px-6 py-3 shadow-[0_4px_4px_-2px_rgba(0,0,0,0.1)]  mb-1.5">
      <div className="flex items-center gap-2">
         <BreadCrumbs />
       </div>
