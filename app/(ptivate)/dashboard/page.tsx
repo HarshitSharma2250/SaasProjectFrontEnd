2633dc0859f3d960c0cec088002cc48cd8dcc144
HarshitSharma2250 <sharma.harshit295@gmail.com>
2025-11-11 16:31:27 +0530

    change shadow

--- app/(ptivate)/dashboard/page.tsx
+++ app/(ptivate)/dashboard/page.tsx
@@ -8,46 +8,44 @@ import TopPerformingUsers from "@/components/sections/TopPerformingUsers";
 
 
 export default function Dashboard() {
-   const trnasection = 200000;
+  const trnasection = 200000;
 
 
 
   return (
-  <div>
+    <div>
       <section >
-<DashBoardSections/>
+        <DashBoardSections />
       </section>
 
-<section className="flex gap-2 mt-10 items-center gap-5 flex-wrap justify-between">
+      <section className="flex gap-2 mt-10 items-center gap-5 flex-wrap justify-between">
 
- <div className="bg-[#fff] p-5 rounded-xl">
-  <h3 className="text-center">Registration Chart</h3>
-   <UserRegistrationChart/>
- </div>
+        <div className="bg-[#fff] p-5 rounded-xl shadow-md">
+          <h3 className="text-center">Registration Chart</h3>
+          <UserRegistrationChart />
+        </div>
 
-<div className="bg-[#fff] p-5 rounded-xl">
-  <h3 className="text-center">Total Transection</h3>
-   <h3 className="font-bold"> ₹ :  {trnasection}</h3>
-  <TransectionVolumeChart/>
-</div>
-
-
-<div className="bg-[#fff] p-5 rounded-xl">
-  <h3 className="text-center font-bold">Top Revinue Generated Users</h3>
-  <TopPerformingUsers/>
-</div>
+        <div className="bg-[#fff] p-5 rounded-xl shadow-md">
+          <h3 className="text-center">Total Transection</h3>
+          <h3 className="font-bold"> ₹ :  {trnasection}</h3>
+          <TransectionVolumeChart />
+        </div>
 
 
+        <div className="bg-[#fff] p-5 rounded-xl shadow-md">
+          <h3 className="text-center font-bold">Top Revinue Generated Users</h3>
+          <TopPerformingUsers />
+        </div>
 
-</section>
+      </section>
 
 
-<section className="flex gap-2 mt-10 items-center gap-5 flex-wrap justify-between">
-  <div className="bg-[#fff] p-5 rounded-xl">
-  <h3 className="text-center font-bold">Exchange Rate Chart</h3>
-  <ExchangeRateChart/>
-</div>
-</section>
+      <section className="flex gap-2 mt-10 items-center gap-5 flex-wrap justify-between">
+        <div className="bg-[#fff] p-5 rounded-xl shadow-md">
+          <h3 className="text-center font-bold">Exchange Rate Chart</h3>
+          <ExchangeRateChart />
+        </div>
+      </section>
 
 
 
